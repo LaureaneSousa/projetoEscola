543fa0c084912887f469abf5c9984dc4cce24077
Laureane Sousa <proflaureanesousa@gmail.com>
2023-10-07 22:39:11 -0300

    js Inscricao

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function CadAluno(){
 
          alert("Cadastro efetuado com sucesso!");
      }
-}
+};
 
 function CadFunc(){
     var nome = document.getElementById("nome");
@@ -57,7 +57,33 @@ function CadFunc(){
 
          alert("Cadastro efetuado com sucesso!");
      }
-}
+};
+
+
+function Inscricao(){
+    var opCurso = document.getElementById("op");
+    var nome = document.getElementById("nome");
+    var dn = document.getElementById("dn");
+    var gen = document.getElementById("dn");
+    var escola = document.getElementById("escola");
+    var mat = document.getElementById("mat");
+    var resp = document.getElementById("resp");
+    var email = document.getElementById("emailCad")
+    var end = document.getElementById("end");
+    var fone = document.getElementById("fone");
+    var pcd = document.getElementById("pcd");
+
+    if (opCurso.value=="" || nome.value =="" || dn.value==""  ||
+            gen.value == "" || escola.value=="" || mat.value=="" ||
+            resp.value=="" || email.value == "" || end.value == "" ||
+            fone.value==""  || pcd.value=="" ){
+
+        alert("Preencha todos os campos!");
+     }else{
+
+         alert("Cadastro efetuado com sucesso!");
+     }
+    };
 
 
 
